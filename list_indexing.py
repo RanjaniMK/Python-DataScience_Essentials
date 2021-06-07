197622426a1850a3fb5a2df873d52ee5bbbb33e7
Ranjani <ranjanimk3@gmail.com>
2021-06-07 02:29:48 +0200

    Create list_indexing.py

---
+++ list_indexing.py
@@ -0,0 +1,8 @@
+# getting the elements from a list using indexing
+
+list = [1,2,3,4,"Chennai", "Paris", "Frankfurt", "New York"]
+#first element: list[0]
+#last element: list[-1]
+
+print(list[0])
+print(list[-1])
