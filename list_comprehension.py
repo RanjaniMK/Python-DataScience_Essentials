0c4456c1db30542458ce56ec659d4eb34773f4ac
Ranjani <ranjanimk3@gmail.com>
2021-06-07 04:20:05 +0200

    Create list_comprehension.py

---
+++ list_comprehension.py
@@ -0,0 +1,8 @@
+my_list = [1, 2, 3, 4, 5]
+
+# alternate way of using loops to perform operations or apply mathematical functions on elements
+# perform squaring of elements i.e. [element ** 2 for element in my_list]
+output_list=[element ** 2 for element in my_list]
+print(output_list)
+
+
