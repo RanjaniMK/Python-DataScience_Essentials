55d33a350426a97b25bc124ad905eafae55befed
Ranjani <ranjanimk3@gmail.com>
2021-06-07 03:56:44 +0200

    Create slice_a_list_OR_to_get_multiple_elements.py

---
+++ slice_a_list_OR_to_get_multiple_elements.py
@@ -0,0 +1,12 @@
+my_list = [2, "Ranjani", 4, "Age", 8, "Data", 10]
+#get all the elements in a list: my_list[:]
+#get 2nd to 4th element: my_list[1:4] i.e. my_list[index1 is 2nd element, mentioning index 4 - but except for index 4, all elements preceding it is printed]
+# 4th element is index 3 in the array. Elements to be retrieved---> then retrieve using indexes, knowing how they are used.
+
+#printing all elements:
+
+print(my_list[:])
+
+#printing 2nd to 4th element:
+
+print(my_list[1:4])
