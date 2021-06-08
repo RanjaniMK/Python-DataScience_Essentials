2b9400ef3af34d482386bad43002ba714f4addf6
Ranjani <ranjanimk3@gmail.com>
2021-06-08 03:48:18 +0200

    Create creating_a_set_from_a_list.py

---
+++ creating_a_set_from_a_list.py
@@ -0,0 +1,11 @@
+# Trying to create a set from a list
+# 1. create a list
+# 2. create a set
+
+my_list = [1,2,3,4,5]
+my_set = set(my_list)
+print(my_set)
+
+#Note: The list has square brackets
+#      The output of a set has curly braces. Like below:
+#      {1, 2, 3, 4, 5}
