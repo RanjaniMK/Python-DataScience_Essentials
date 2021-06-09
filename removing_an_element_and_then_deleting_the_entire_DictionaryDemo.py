2023f5ec98fcc23e6308039dffdc62f70075b3a1
Ranjani <ranjanimk3@gmail.com>
2021-06-09 23:08:48 +0200

    Create removing_an_element_and_then_deleting_the_entire_DictionaryDemo.py

---
+++ removing_an_element_and_then_deleting_the_entire_DictionaryDemo.py
@@ -0,0 +1,17 @@
+#Dictionary creation
+
+dictionary = {"Name":"Ranj",
+              "Sex":"Female"}
+
+print(dictionary)
+
+#removing a key:value pair
+del dictionary["Name"]
+print(dictionary)
+
+#deleting the entire dictionary
+del dictionary
+print(dictionary)
+
+#The output after deleting the dictionary is, "NameError: name 'dictionary' is not defined"
+
