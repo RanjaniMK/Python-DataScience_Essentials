ea3a8d39a2c0f2b49d230ee2fcb42ae3229cfbb8
Ranjani <ranjanimk3@gmail.com>
2021-06-09 22:58:20 +0200

    Create creating_accessing_and_adding_elements_to_a_Dictionary.py

---
+++ creating_accessing_and_adding_elements_to_a_Dictionary.py
@@ -0,0 +1,28 @@
+#In Dictionary: Elements are stored in a key-value pair format.
+
+#Dictionary elements are accessed via keys like how List elements are accessed by their respective index.
+
+
+# Defining a dictionary:
+
+ranj_dict = {"Name" : "Ranjani",
+             "Sex": "Female",
+             "Occupation": "Engineer",
+            "Current_Year": 2021}
+
+
+#Accessing a dictionary element by using it's key and printing out the same later:
+ranj_dict["Name"]
+
+
+print(ranj_dict["Name"])
+#Ouput: Ranjani
+
+#Adding a key-value pair:
+ranj_dict["University"]="TU Darmstadt"
+
+#Printing out the dictionary key-value pairs to check if the newest k,v pair -> University: TU Darmstadt is added.
+print(ranj_dict)
+
+#Output:{'Name': 'Ranjani', 'Sex': 'Female', 'Occupation': 'Engineer', 'Current_Year': 2021, 'University': 'TU Darmstadt'}
+
