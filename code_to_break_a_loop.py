5bd5b13342a22d7c016c933774df313cb730c284
Ranjani <ranjanimk3@gmail.com>
2021-06-10 19:51:16 +0200

    Create code_to_break_a_loop.py

---
+++ code_to_break_a_loop.py
@@ -0,0 +1,9 @@
+#Code to break a loop
+
+my_list = ["apple", "mango", "lemon", "orange", "pineapple"]
+for i in my_list:
+    print(i)
+    if i== "lemon":
+        break
+
+#Output: apple mango lemon (columnar display of data)
