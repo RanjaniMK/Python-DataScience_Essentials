49d21bd0dcbb146186a9557a7c006079012f6854
Ranjani <ranjanimk3@gmail.com>
2021-06-10 03:47:48 +0200

    Create Multiple_if_else_statements_Demo.py

---
+++ Multiple_if_else_statements_Demo.py
@@ -0,0 +1,8 @@
+if 5==7:
+    print('first statment is true.')
+elif 2==4:
+    print('second statement is true.')
+else:
+    print('last statement is true.')
+    
+    #Output: last statement is true.
