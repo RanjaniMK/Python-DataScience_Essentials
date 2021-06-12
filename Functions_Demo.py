44fd68442289aa2d75f223d9a9d9cecb836c06bd
Ranjani <ranjanimk3@gmail.com>
2021-06-12 13:51:03 +0200

    Create Functions_Demo.py

---
+++ Functions_Demo.py
@@ -0,0 +1,18 @@
+# A function is a block of code that can run whenever it is called.
+# Information passed to a function is called an argument.
+
+#FUNCTION DEFINITION: Using a variable
+
+def squared(x):
+    return x**2
+
+#FUNCTION CALL: Called by passing values/numbers/integers as parameter here
+
+print(squared(2))
+print(squared(4))
+
+#Output: The following lines were the output and should be considered as comments.
+4
+16
+
+Process finished with exit code 0
