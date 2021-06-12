ffc20febb205f37cf42993a0343cad8c75d037c8
Ranjani <ranjanimk3@gmail.com>
2021-06-12 17:58:13 +0200

    Create file_creation.py

---
+++ file_creation.py
@@ -0,0 +1,14 @@
+#Creating a file
+
+f=open("RANJ.txt", "x")
+f.write("Hello, I am Ranjani.")
+f=open("RANJ.txt")
+f.close()
+
+#Output: 
+
+A file with the name RANJ.txt is created and found inside the project directory located in the PyCharm IDE. And it has the following contents:
+  
+  Hello, I am Ranjani.
+
+Process finished with exit code 0
