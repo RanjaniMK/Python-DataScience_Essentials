cc234e7ca4e3fbc9ee808c0e13e58758d4af160b
Ranjani <ranjanimk3@gmail.com>
2021-06-12 16:30:37 +0200

    Create Lambda_Anonymous_Function_Demo.py

---
+++ Lambda_Anonymous_Function_Demo.py
@@ -0,0 +1,20 @@
+#A typical function with a name can be written as below:
+#def squared(x):
+#    return x**2
+
+#FUNCTION CALL:
+
+#print(squared(2))
+#print(squared(4))
+
+# Executing the above same function alternatively using an anonymous function called lambda
+
+y= lambda x:x**2
+
+#Calling the function
+y(4)
+print(y(4))
+print(y(10))
+
+
+#Output: 16    100 (Columnar Display)
