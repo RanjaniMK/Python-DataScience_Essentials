4b88d1e1cf34a9533ad1b95def14406fea7b393d
Ranjani <ranjanimk3@gmail.com>
2021-06-12 18:08:23 +0200

    Create Appending_to_a_file.py

---
+++ Appending_to_a_file.py
@@ -0,0 +1,14 @@
+f=open("RANJ.txt", "a")
+f.write("I am handling files in Python.")
+f.close()
+
+
+#The file RANJ.txt already exists with the content: Hello, I am Ranjani.
+#Output: 
+Process finished with exit code 0
+
+Now when you check the file RANJ.txt, you can see that the data, " I am handling files in Python." is appended to the file:
+  
+  Appears as:
+    
+    Hello, I am Ranjani.I am handling files in Python.
