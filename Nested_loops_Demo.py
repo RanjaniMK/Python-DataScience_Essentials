5f8c537d69edb3b130484e2a36f32a6b434c2029
Ranjani <ranjanimk3@gmail.com>
2021-06-12 13:35:45 +0200

    Create Nested_loops_Demo.py

---
+++ Nested_loops_Demo.py
@@ -0,0 +1,64 @@
+#To print the multiplication table
+for x in range(1, 6):
+    for y in range(1, 11):
+        print('{}*{}={}'.format(x, y, x*y) )
+        
+        
+        
+#First for loop : for vertical display/range
+#Second for loop: for horizontal display/range
+#range(1,6) : means 1,2,3,4,5 included, and 6 is excluded
+#range(1,11) : means 1,2,3,4,5,6,7,8,9,10 included, and 11 is excluded
+#Output: The following lines were the output(not the code itself), and hence shall be considered as comments with an # prefixed. 
+1*1=1
+1*2=2
+1*3=3
+1*4=4
+1*5=5
+1*6=6
+1*7=7
+1*8=8
+1*9=9
+1*10=10
+2*1=2
+2*2=4
+2*3=6
+2*4=8
+2*5=10
+2*6=12
+2*7=14
+2*8=16
+2*9=18
+2*10=20
+3*1=3
+3*2=6
+3*3=9
+3*4=12
+3*5=15
+3*6=18
+3*7=21
+3*8=24
+3*9=27
+3*10=30
+4*1=4
+4*2=8
+4*3=12
+4*4=16
+4*5=20
+4*6=24
+4*7=28
+4*8=32
+4*9=36
+4*10=40
+5*1=5
+5*2=10
+5*3=15
+5*4=20
+5*5=25
+5*6=30
+5*7=35
+5*8=40
+5*9=45
+5*10=50
+
+Process finished with exit code 0
