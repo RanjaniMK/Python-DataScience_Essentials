0a6c6415525be4b0812a536c42f32efafbb2c91c
Ranjani <ranjanimk3@gmail.com>
2021-06-12 14:08:45 +0200

    Create Functions_Demo_using_a_default_parameter_value.py

---
+++ Functions_Demo_using_a_default_parameter_value.py
@@ -0,0 +1,11 @@
+def my_function(age=6):
+    return("I am {} years old".format(age))
+
+
+print(my_function())
+
+
+#Output:The following lines were the output and should be considered as comments going forward.
+I am 6 years old
+
+Process finished with exit code 0
